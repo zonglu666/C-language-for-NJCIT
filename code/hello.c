937ce1734e1a65f811b58ee93609517885abc065
Lu Zong <415390588@qq.com>
2020-03-18 04:10:07 +0800

    commit

---
+++ code/hello.c
@@ -0,0 +1,24 @@
+/**
+ * 编译过程
+ * 1.预编译(编译预处理) 生成预处理文件
+ * gcc -E hello.c -o hello.i
+ * 
+ * 2.编译 编译器gcc把预处理后的文件进行语法分析、语义分析以及优化后生成汇编代码文件
+ * gcc -S hello.i -o hello.s
+ * 
+ * 3.汇编 将汇编文件编译成中间目标文件
+ * as hello.s -o hello.o
+ * 
+ * 4.链接(生成可执行文件) 将中间目标文件与需要的附加目标文件(如静态链接库、动态链接库)链接起来，生成可执行文件
+ * gcc hello.c -o hello   #
+ * 
+ * 5.执行可执行文件
+ * ./hello
+ */
+#include <stdio.h>
+int main()
+{
+    printf("11111\n22222\t33333\b44444\r55555\\66666\"77777\'");
+    printf("Hello,world!\n");
+    return 0;
+}
---
+++ code/hello.s
@@ -0,0 +1,41 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 12
+	.globl	_main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Lcfi0:
+	.cfi_def_cfa_offset 16
+Lcfi1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Lcfi2:
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	leaq	L_.str(%rip), %rdi
+	movl	$0, -4(%rbp)
+	movb	$0, %al
+	callq	_printf
+	leaq	L_.str.1(%rip), %rdi
+	movl	%eax, -8(%rbp)          ## 4-byte Spill
+	movb	$0, %al
+	callq	_printf
+	xorl	%ecx, %ecx
+	movl	%eax, -12(%rbp)         ## 4-byte Spill
+	movl	%ecx, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"11111\n22222\t33333\b44444\r55555\\66666\"77777'"
+
+L_.str.1:                               ## @.str.1
+	.asciz	"Hello,world!\n"
+
+
+.subsections_via_symbols
